c5332ed606a19fd54bf6d0d6695a259d372ae228
Makornthawat Emery <makornthawateugeneemery@gmail.com>
2018-06-21 18:17:45 +0530

    power: qpnp-smbcharger: Fix if statement has empty body warning

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -10582,7 +10582,7 @@ void read_BR_countrycode_work(struct work_struct *work)
 	fd = filp_open(COUNTRY_CODE_PATH, O_RDONLY, 0);
 	if (IS_ERR_OR_NULL(fd)) {
         printk("[BAT][CHG] OPEN (%s) failed\n", COUNTRY_CODE_PATH);
-		if (BR_read_count <= 10); {
+		if (BR_read_count <= 10) {
 			schedule_delayed_work(&read_countrycode_work, msecs_to_jiffies(2000));
 			BR_read_count ++;
 		}
